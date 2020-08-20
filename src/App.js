248874bac6e81bcadf0248860a8d47a7e366a5d0
Rakshith Rangaswamy <rakshith.nrr@gmail.com>
2020-08-20 16:08:14 -0700

    adding collection item and styles

--- src/App.js
+++ src/App.js
@@ -1,67 +1,19 @@
 import React from 'react';
-import {Route, Link} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 
 import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats PAGE</h1>
-  </div>
-);
-
-const JacketsPage = () => (
-  <div>
-    <h1>Jackets PAGE</h1>
-  </div>
-);
-
-const SneakersPage = () => (
-  <div>
-    <h1>Sneakers PAGE</h1>
-  </div>
-);
-
-const WomensPage = () => (
-  <div>
-    <h1>Womens PAGE</h1>
-  </div>
-);
-
-const MensPage = () => (
-  <div>
-    <h1>Mens PAGE</h1>
-  </div>
-);
-
-const TopicsList = props => {
-  return (
-    <div>
-      <h1>TOPIC LIST PAGE</h1>
-      <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
-      <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link>
-      <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link>
-    </div>
-  );
-};
-
-const TopicDetail = props => {
-  return (
-    <div>
-      <h1>TOPIC V1 PAGE: {props.match.params.topicId}</h1>
-    </div>
-  );
-};
-
-
 function App() {
   return (
     <div >
+      <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/shop/hats' component={HatsPage}/>
-        <Route exact path='/topics/:topicId' component={TopicDetail} />        
+        <Route exact path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
